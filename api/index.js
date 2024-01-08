30cc1be1918a60551b834c753fadc60b946e92ef
nimacortez <cortez.nima@gmail.com>
2024-01-08 18:37:26 -0500

    added mongodb database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+const mongoUrl = 'mongodb+srv://corteznima:JdsD82Tk5zpyEXsp@cluster0.qddx7os.mongodb.net/?retryWrites=true&w=majority';
+
 const app = express();
 
 app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
+app.post('/register', (req, res) => {
+  res.json('register ok'); 
+});
+
 app.listen(4000); // Port 4000
+
+//JdsD82Tk5zpyEXsp
